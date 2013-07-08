e287ba8a66ea92f5cd6cbd98ebf12948ed190a8d
Tung Phan <nlds.a279@gmail.com>
2013-07-08 14:20:02 +0700

    Finish user edit , update , index , and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    store_previous_location
   end
 
   def create
@@ -7,7 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
     # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to user
+      # friendly forwarding
+      redirect_back_or user
     else
     # Create an error message and re-render the signin form.
       flash.now[:error] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end  
+
   def current_user=(user)
     @current_user = user
   end
@@ -10,11 +11,31 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+
   def signed_in?
 	  !current_user.nil?
   end
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def store_previous_location
+    session[:previous_page] = request.referer
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || session[:previous_page] || default)
+    session.delete(:return_to)
+    session.delete(:previous_page)
   end
 end
