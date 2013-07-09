21179402d23db51286faf2a6b72264285eab35ad
Tung <tung@ubuntu.ubuntu-domain>
2013-07-09 05:52:19 -0400

    Followed users and Followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
-    before_filter :signed_in_user, only: [:index,:edit, :update, :destroy]
+    before_filter :signed_in_user, only: [:index,:edit, :update, :destroy, :following, :followers]
     before_filter :correct_user, only: [:edit, :update]
     before_filter :admin_user,  only: :destroy
     before_filter :new_user, only: [:new,:create]
     
 	  def show
-    	@user = User.find(params[:id])
-      @microposts = @user.microposts.paginate(page: params[:page],per_page: 10)
+    	@user = User.find_by_id(params[:id])
+      if !@user.nil?
+        @microposts = @user.microposts.paginate(page: params[:page],per_page: 10)
+      else
+        redirect_to(root_path)
+      end
   	end
 
   	def new
@@ -50,9 +54,21 @@ class UsersController < ApplicationController
       redirect_to users_url
     end
 
-    private
+    def following
+      @title = "Following"
+      @user = User.find(params[:id])
+      @users = @user.followed_users.paginate(page: params[:page], per_page: 10)
+      render 'show_follow'
+    end
 
-      
+    def followers
+      @title = "Followers"
+      @user = User.find(params[:id])
+      @users = @user.followers.paginate(page: params[:page], per_page: 10)
+      render 'show_follow'
+    end
+
+    private      
 
       def correct_user
         @user = User.find(params[:id])
